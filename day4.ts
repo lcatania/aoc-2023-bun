854255e5da9d6f7ae43ce7de909a907f728b278f
Lorenzo Catania <lorenzo.catania@dampsoft.de>
2023-12-04 13:54:30 +0100

    Rewrote day4 part 2

--- day4.ts
+++ day4.ts
@@ -17,47 +17,23 @@ export function day4(input: string) {
 }
 
 export function day4part2(input: string) {
-    let result = 0;
-    const allScratchpads: { [key: number]: number } = {}
-    const cards = input.split("\n");
-    cards.forEach((card, cardIndex) => {
-        const cardNumber = Number(card.split(":")[0].replace("Card", "").trim())
-        console.log(cardNumber)
-        const splittedLine = card.split(":")[1].split("|")
-        const winningNumbers = splittedLine[0].trimStart().trimEnd().split(" ").map(Number);
-        const numbers = splittedLine[1].trimStart().trimEnd().split(" ").filter(n => n !== "").map(Number)
-        let matches = 0
-        let cards = 1
-        if (allScratchpads[cardNumber] !== undefined) {
-            cards = allScratchpads[cardNumber];
-        }
-        for (let index = 0; index < cards; index++) {
-            let wonCards = [];
-            numbers.forEach((n) => {
-                if (winningNumbers.includes(n)) {
-                    matches += 1
-                }
-            })
-            if (matches !== 0) {
-                for (let i = cardNumber + 1; i <= cardNumber + matches; i++) {
-                    wonCards.push(i);
-                }
-            }
-
-            wonCards.forEach((card) => {
-                if (card in allScratchpads) {
-                    allScratchpads[card] = allScratchpads[card] + 1;
-                } else {
-                    allScratchpads[card] = 2;
-                }
-            })
-            if (!(cardNumber in allScratchpads)) {
-                allScratchpads[cardNumber] = 1;
-            }
+    let result: { [key: string]: number } = {}
+    let p1 = 0
+    input.split("\n").forEach((line, i) => {
+        if (!result[i])
+            result[i] = 1
+        else
+            result[i] += 1;
+        const cards = line.split('|')[0].split(":")[0]
+        const winningNumbers = line.split('|')[0].split(":")[1].trimStart().trimEnd().split(" ").filter(n => n !== "").map(Number);
+        const matchingNumbers = line.split('|')[1].trimStart().trimEnd().split(" ").filter(n => n !== "").map(Number);
+        const value = new Set([...matchingNumbers].filter(x => new Set([...winningNumbers]).has(x))).size;
+        if (value > 0)
+            p1 += 2 ** (value - 1)
+        for (let j = 0; j < value; j++) {
+            result[(i + 1 + j)] = (result[(i + 1 + j)] ?? 0) + result[i]
         }
     })
-    for (const [, value] of Object.entries(allScratchpads)) {
-        result += value;
-    }
-    return result
+    console.log(p1)
+    console.log(Object.values(result).reduce((a, b) => a + b))
 }
